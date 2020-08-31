7e81eda1594fb5d65841eb6e9069b17c6a6be257
Alexey Yashin <ayashin@extyl-pro.ru>
2020-08-31 15:20:15 +0300

    iblock property

---
+++ src/AbstractDto.php
@@ -0,0 +1,119 @@
+<?php
+namespace AlexeyyYashin\Bitrix\Dto;
+
+use ArrayIterator;
+use ArrayObject;
+
+/**
+ * Class AbstractDto
+ * @package Malltech\Dto
+ */
+abstract class AbstractDto extends ArrayObject
+{
+    /**
+     * @var bool
+     */
+    protected static $array_upper = false;
+    /**
+     * @var bool
+     */
+    protected static $filter_nulls = false;
+
+    protected static $tilda = false;
+
+    /**
+     * AbstractDto constructor.
+     *
+     * @param array $array
+     */
+    public function __construct($array = [])
+    {
+        $array = array_change_key_case($array, CASE_LOWER);
+
+        foreach ($array as $key => $value) {
+            if (static::$tilda) {
+                $key = preg_replace('/^~/', '', $key);
+            }
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            } else {
+                unset($array[$key]);
+            }
+        }
+
+        parent::__construct($this->getArrayCopy(), ArrayObject::STD_PROP_LIST/* | \ArrayObject::ARRAY_AS_PROPS*/);
+    }
+
+    /**
+     * @return array
+     */
+    public function getArrayCopy(): array
+    {
+        $array = $this->mkArray();
+
+        $this->exchangeArray($array);
+
+        return $array;
+    }
+
+    /**
+     * @return array
+     */
+    private function mkArray(): array
+    {
+        $array = get_object_vars($this);
+
+        if (static::$filter_nulls) {
+            $array = array_filter($array, function ($v)
+            {
+                return $v !== null;
+            });
+        }
+
+        if (static::$array_upper) {
+            $array = array_change_key_case($array, CASE_UPPER);
+        }
+
+        return $array;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return $this->getArrayCopy();
+    }
+
+    /**
+     * @return \ArrayIterator
+     */
+    public function getIterator(): ArrayIterator
+    {
+        return new ArrayIterator((array)$this->getArrayCopy());
+    }
+
+    /**
+     * @param mixed $index
+     *
+     * @return mixed
+     */
+    public function offsetGet($index)
+    {
+        $index = mb_strtolower($index);
+        $this->getArrayCopy();
+
+        return $this->{$index};
+    }
+
+    /**
+     * @param mixed $index
+     * @param mixed $newval
+     */
+    public function offsetSet($index, $newval)
+    {
+        $this->{mb_strtolower($index)} = $newval;
+        parent::offsetSet($index, $newval);
+        $this->getArrayCopy();
+    }
+}
---
+++ src/Iblock/PropertyDto.php
@@ -0,0 +1,43 @@
+<?php
+namespace AlexeyYashin\Bitrix\Dto\Iblock;
+
+use AlexeyyYashin\Bitrix\Dto\AbstractDto;
+
+class PropertyDto extends AbstractDto
+{
+    protected static $array_upper = true;
+
+    protected static $tilda = true;
+
+    public $id;
+    public $code;
+    public $xml_id;
+    public $iblock_id;
+    public $name;
+    public $active = 'Y';
+    public $is_required = 'N';
+    public $sort = 500;
+    public $property_type = 'S';
+    public $multiple = 'N';
+    public $timestamp_x;
+    public $default_value;
+    public $row_count = 1;
+    public $col_count = 30;
+    public $list_type = 'L';
+    public $multiple_cnt;
+    public $file_type;
+    public $searchable = 'N';
+    public $filterable = 'N';
+    public $link_iblock_id;
+    public $with_description;
+    public $version = 1;
+    public $user_type;
+    public $user_type_settings;
+    public $hint;
+
+    public $value;
+    public $value_enum;
+    public $value_enum_id;
+    public $value_xml_id;
+    public $value_sort;
+}
